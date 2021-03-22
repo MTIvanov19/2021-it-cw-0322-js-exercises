e478a405c7eece0488d9dab70c9e51580b8d4245
Miroslav Ivanov <MTIvanov19@codingburgas.bg>
2021-03-22 11:36:50 +0200

    Implement lab4

---
+++ MTIvanov19/lab4.js
@@ -0,0 +1,16 @@
+function ask(a, yes, no)
+{
+    if(a < 18)
+    {
+        yes();
+    }
+    else
+    {
+        no();
+    }
+}
+
+let yesFunc = () => console.log("yes");
+let noFunc = () => console.log("no");
+
+ask(0, yesFunc, noFunc);
